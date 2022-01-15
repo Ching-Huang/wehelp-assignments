f3e5566bb487fc2db8364259a47d48e8bf8a1fe0
Ching-Huang <eileenhuang214@gmail.com>
2022-01-15 22:31:58 +0800

    新增第三周作業的第三題

--- week-3/JS/sight.js
+++ week-3/JS/sight.js
@@ -1,31 +1,28 @@
-async function getSight(){
-    let object = await fetch("https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment.json");
-    object = await object.json();
-    sightData = object.result.results;
-    // console.log(sightData);
-    let sightName; /*文字*/
-    let sightPic; /*圖片*/
-    for(let i = 0; i < 8; i++){
-        /*文字*/
+/***************************************************************  
+    runder   函數=>從sightData陣列中新增每8張景點圖片到網頁上
+    picIndex 參數=>sightData陣列中的每張圖片索引值(index:0~57)
+***************************************************************/
+function runder(picIndex){
+    for(let i = picIndex; i < (picIndex+8); i++){
+        /*索引值超過景點筆數就跳出*/
+        if(i >= sightData.length){
+            break;
+        }
+
+        /*文字資料*/
         sightName = sightData[i].stitle; 
-        // console.log(sightName);
         /*新增p標籤*/
         let p = document.createElement('p');
         let newText = document.createTextNode(sightName);
-        // console.log(newText);
-        p.appendChild(newText);/*文字要去除兩邊雙引號!*/
+        p.appendChild(newText);
         /*p設置class屬性*/
         p.classList.add('cap');
-        // console.log(p); 
+        
 
-        /*圖片*/
+        /*圖片資料*/
         sightPic = sightData[i].file;
-        // console.log(sightPic);
         let picLink = sightPic.split('https://');
-        // console.log(picLink);
         let firstPicLink = 'https://'+ picLink[1];
-        // console.log(firstPicLink);
-
         /*新增img標籤*/
         let img = document.createElement('img');
         /*img設置src屬性*/
@@ -43,29 +40,45 @@ async function getSight(){
         let height = document.createAttribute('height');
         height.value = '200px';
         img.setAttributeNode(height);
-        
-        // console.log(img);
 
         /*新增div標籤*/
         let div = document.createElement('div'); 
         /*div設置class屬性*/
-        div.classList.add("cards");
-
+        div.classList.add('cards');
         /*div新增img、p子節點*/
         div.appendChild(img);
         div.appendChild(p);
-        // console.log(div);
-
+        
         /*取得section父節點位置*/
         let section = document.querySelector('.images');
-
         /*section新增div子節點*/
         section.appendChild(div); 
-        // console.log(section);
-
-         
+        
     }
-       
+}
+
+
+async function getSight(){
+    let object = await fetch("https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment.json");
+    object = await object.json();
+    sightData = object.result.results;
+
+    let sightName; /*文字*/
+    let sightPic; /*圖片*/
+
+    /*************************************** 
+        綁監聽按鈕:每點擊一次，就會新增8張圖片
+    ****************************************/
+    let picStartIdx = 8;
+    let btn = document.querySelector('#btn');
+    btn.addEventListener('click', e => {
+        runder(picStartIdx);
+        picStartIdx += 8;
+    })
+
+    /*網頁初始的8張景點圖片*/
+    runder(0);
+    
 }
 getSight();
 
